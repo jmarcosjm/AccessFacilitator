4e0329aece05818622cc9d4a74c8c8daac187dab
juau <jmarcosomjm@gmail.com>
2021-07-07 14:40:54 -0300

    update

--- access.js
+++ access.js
@@ -4,26 +4,32 @@ onload = () => {
         let database = document.getElementById("db").value
         let perms = document.getElementById("perm").value
         let schemas = document.getElementById("schema").value
-        inputHander(database,perms,schemas)
+        let user = document.getElementById("user").value
+        inputHander(database, perms, schemas, user)
     }
 }
 
-function inputHander(database, perms, schemas){
+function inputHander(database, perms, schemas, user){
     perms = perms.split(",")
     schemas = schemas.split(",")
-    outputHandler(database,perms,schemas)
+    outputHandler(database, perms, schemas, user)
 }
 
-function outputHandler(database, perms, schemas){
+function outputHandler(database, perms, schemas, user){
     let screen = document.getElementById("screen")
     screen.innerHTML = ""
+    let access = "GRANT"
+    let i = 0;
     schemas.forEach(schema => {
         if(schema != "") schema = "_"+schema.toUpperCase();
         perms.forEach(perm => {
-            screen.innerHTML += 
-                `
-                <p>${("PG_"+perm.toUpperCase()+schema+"_"+database.toUpperCase()).replaceAll(" ","")}</p>
-                `
-        })    
+            access += (i == 0)? ` ${('"PG_'+perm.toUpperCase()+schema+"_"+database.toUpperCase()).replaceAll(" ","")+'"'}` : `, ${('"PG_'+perm.toUpperCase()+schema+"_"+database.toUpperCase()).replaceAll(" ","")+'"'}`
+            i++
+        })   
     })
+    access += ` TO "${user}";`
+    screen.innerHTML += 
+    `
+    <p>${access}</p>
+    ` 
 }
